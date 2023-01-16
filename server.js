5a46e738e9213850714bc312e6b377a0cd3c9290
fyrkantis <david@kniberg.com>
2023-01-16 23:04:14 +0100

    Added website and basic file sending capabliities.

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 "use strict";
 var http = require("http");
-var port = process.env.PORT || 5000;
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var port = process.env.PORT || 1337;
 
 http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Your path: " + request.url);
+    console.log("URL: " + request.url)
+    if (request.url == "/") {
+        response.writeHead(308, "Permanent redirect", { "Location": "/index.html" });
+        response.end();
+        return;
+    }
+    let filePath = path.join(__dirname, "website", request.url);
+    console.log(filePath)
+    if (!fs.existsSync(filePath)) {
+        response.writeHead(404, "Not Found");
+        response.end();
+        return;
+    }
+    try {
+        let body = fs.readFileSync(filePath);
+        response.writeHead(200, "OK", { "Content-Type": "text/html" });
+        response.end(body);
+    } catch (error) {
+        response.writeHead(500, "Internal Server Error", { "Content-Type": "text/plain" });
+        response.end(error);
+    }
 }).listen(port);
